38153fd0e6351db4c1254356719aef3fedf99f9a
jebeene <89660618+jebeene@users.noreply.github.com>
2025-03-30 14:38:21 -0400

    fix data.py and create slurm script

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 from config import LABEL2ID
 
 def load_liar_dataset():
-    dataset = load_dataset("liar")
+    dataset = load_dataset("liar", trust_remote_code=True)
     for split in dataset:
         dataset[split] = dataset[split].filter(lambda x: x["label"] != "")
         dataset[split] = dataset[split].map(lambda x: x)
